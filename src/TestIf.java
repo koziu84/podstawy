16f1dfb4f129091ef3a87cd279f938c5c19a984c
koziu84 <gkoziu84@gmail.com>
2014-06-26 23:06:07 +0200

    TestIf

--- src/TestIf.java
+++ src/TestIf.java
@@ -1,4 +1,40 @@
+import java.util.Scanner;
 
 public class TestIf {
+
+	public static void main(String[] args) {
+
+		// System.out.println("Pierwsza liczba: ");
+		// Scanner sc = new Scanner(System.in);
+		// int a = sc.nextInt();
+		//
+		// System.out.println("Druga liczba: ");
+		// int b = sc.nextInt();
+		//
+		// sc.close();
+		//
+		// if (a == b) {
+		// System.out.println("Liczby równe");
+		// } else {
+		// System.out.println("Liczby nierówne");
+		// }
+
+		System.out.println("Podaj imie: ");
+		Scanner sc = new Scanner(System.in);
+		String imie = sc.next();
+
+		switch (imie) {
+		case "marek":
+			System.out.println("Imie to Marek");
+			break;
+		case "jacek":
+			System.out.println("Imie to Jacek");
+		case "michal":
+			System.out.println("Imie to Michal");
+		default:
+			System.out.println("Zadne imie");
+		}
+
+	}
 
 }
